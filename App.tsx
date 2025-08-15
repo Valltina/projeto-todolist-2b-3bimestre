0588275b051efdd60744fdc76292500a619acf7d
Valentina de Oliveira Mendes <valentinaoliveira918@gmail.com>
2025-08-15 11:09:14 -0300

    [feat] Style of App.js

--- App.tsx
+++ App.tsx
@@ -1,10 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { Image, StyleSheet, Text, View } from 'react-native';
+import logo from './assets/todoicon.png';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <View>
+        <Image source={logo} style={styles.logo} />
+      </View>
+      <Text>Projeto 2B 15/08</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -16,5 +19,10 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+   
   },
+   logo:{
+    height: 128,
+    width : 128,
+   },
 });
